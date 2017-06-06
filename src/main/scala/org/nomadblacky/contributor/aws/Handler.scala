1c92fc4be084ac4f28ee408b913d9b2b9ae065c0
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-06-07 00:13:00 +0900

    Add AWS Lambda handler

--- src/main/scala/org/nomadblacky/contributor/aws/Handler.scala
+++ src/main/scala/org/nomadblacky/contributor/aws/Handler.scala
@@ -1,6 +1,7 @@
 package org.nomadblacky.contributor.aws
 
-import com.amazonaws.services.lambda.runtime.{ Context, RequestHandler }
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import org.nomadblacky.contributor.Contributor
 
 import scala.beans.BeanProperty
 
@@ -8,8 +9,15 @@ import scala.beans.BeanProperty
  * Created by blacky on 17/05/29.
  */
 class Handler extends RequestHandler[Request, Response] {
+  import com.github.nscala_time.time.Imports._
+
   override def handleRequest(input: Request, context: Context): Response = {
-    new Response()
+    val now = DateTime.now
+    val msg =
+      Contributor.getDataCount(input.userName, now)
+        .map(c => s"$c")
+        .getOrElse(s"Not found: ${now.toString("yyyy-MM-dd")}")
+    Response(msg)
   }
 }
 
